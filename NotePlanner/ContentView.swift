d6216921361e6472439f4c8233365e829b6354d4
Joseph DeWeese <jdeweese@weldtascics.systems>
2024-02-12 18:41:41 -0600

    Initial Commit

---
+++ NotePlanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotePlanner
+//
+//  Created by J. DeWeese on 2/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotePlanner/NotePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotePlannerApp.swift
+//  NotePlanner
+//
+//  Created by J. DeWeese on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NotePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
